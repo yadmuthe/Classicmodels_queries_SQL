a8c37e5bd659ae17ed6e12f67df7461dd27e2ff5
yadmuthe <46523034+yadmuthe@users.noreply.github.com>
2021-03-12 16:34:36 +0530

    Update query10.sql

--- Queries Sol./Regular Expressions/query10.sql
+++ Queries Sol./Regular Expressions/query10.sql
@@ -1,2 +1,5 @@
 -- List the names of employees with non-alphabetic characters in their names.
 
+SELECT CONCAT(firstName,' ',lastName) AS 'Full_Name'
+FROM Employees
+WHERE CONCAT(firstName,' ',lastName) RLIKE '[0-9]'
