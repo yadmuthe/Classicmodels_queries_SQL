5d17b9888cdfdfe573fa4d20427b25d63a2910c3
yadmuthe <46523034+yadmuthe@users.noreply.github.com>
2021-03-13 12:40:11 +0530

    Update query.1.sql

--- Queries Sol./One to many relationship/query.1.sql
+++ Queries Sol./One to many relationship/query.1.sql
@@ -1,1 +1,6 @@
+-- Report the account representative for each customer.
 
+SELECT customerName,concat(employees.firstName,' ',employees.lastName) As 'Account Repersentative'
+from customers
+inner join employees on customers.salesRepEmployeeNumber = employees.employeeNumber
+ORDER BY customerName
